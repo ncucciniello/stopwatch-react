6db28883226f2c59c723e87fa3e9a4bd183b99d4
ncucciniello <ncucciniello@gmail.com>
2021-07-13 14:34:16 -0400

    add optimizations?

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,41 @@ const formatTime = (timeInMilli) => {
     totalSeconds % 60,
     (timeInMilli % 1000) / 10
   ].map((num) => Math.floor(num).toString(10).padStart(2, '0'))
-
+  
   return `${minutes}:${seconds}.${centiseconds}`
+}
+
+const displayLaps = (lapsArray, longestLap, shortestLap) => {
+  return lapsArray.map((lap) => {
+    return (
+      <li
+        key={lap.lapNum}
+        className={lapsArray.length > 2 ? handleLapClass(lap, longestLap, shortestLap): ''}
+      >
+        Lap {lap.lapNum} <span>{formatTime(lap.lapTime)}</span>
+      </li>
+    )
+  })
+}
+const handleLapClass = (currentLap, longestLap, shortestLap) => {
+  if (currentLap.lapNum === longestLap.lapNum) {
+    return 'longest'
+  } else if (currentLap.lapNum === shortestLap.lapNum) {
+    return 'shortest'
+  } else {
+    return ''
+  } 
+}
+
+const displayEmptyLaps = (lapsArray) => {
+  const maxNumOfEmptyLaps = 6
+  const numOfEmptyLaps = maxNumOfEmptyLaps - lapsArray.length
+
+  const emptyLaps = [...Array(numOfEmptyLaps)].map((_, index) => {
+    return <li key={index + maxNumOfEmptyLaps}></li> 
+  })
+
+  return emptyLaps
 }
 
 function App() {
@@ -18,8 +51,8 @@ function App() {
 
   const [laps, setLaps] = useState([])
   const [totalLapDuration, setTotalLapDuration] = useState(0)
-  const [shortestLap, setShortestLap] = useState({ lapNum: 0, lapTime: Number.MAX_VALUE })
   const [longestLap, setLongestLap] = useState({ lapNum: 0, lapTime: 0 })
+  const [shortestLap, setShortestLap] = useState({ lapNum: 0, lapTime: Number.MAX_VALUE })
 
   useEffect(() => {
     if (startTime) {
@@ -27,114 +60,101 @@ function App() {
       return () => clearInterval(intervalID)
     }
   }, [startTime])
-  
+
   useEffect(() => {
     if (timeElapsed) {
       runLap()
     }
   }, [timeElapsed])
-  
+
   const startStopwatch = () => {
     if (!timeElapsed) {
       addLap()
     }
-    
+
     setStartTime(Date.now())
   }
-  
+
   const runTimer = () => setTimeElapsed(timeElapsed + (Date.now() - startTime))
-  
+
   const runLap = () => {
     const newLapsArray = [...laps]
     const currentLap = newLapsArray[0]
-    
+
     currentLap.lapTime = timeElapsed - totalLapDuration
-    
+
     setLaps(newLapsArray)
   }
-  
+
   const addLap = () => {
     const newLaps = [{ lapNum: (laps.length + 1), lapTime: 0 }, ...laps]
-    
+
     if (laps.length > 0) {
-      newLaps[1].lapTime > longestLap.lapTime && setLongestLap({lapNum: newLaps[1].lapNum, lapTime: newLaps[1].lapTime})
-      newLaps[1].lapTime < shortestLap.lapTime && setShortestLap({lapNum: newLaps[1].lapNum, lapTime: newLaps[1].lapTime})
+      if (newLaps[1].lapTime > longestLap.lapTime) {
+        setLongestLap({ lapNum: newLaps[1].lapNum, lapTime: newLaps[1].lapTime })
+      }
+      if (newLaps[1].lapTime < shortestLap.lapTime) {
+        setShortestLap({ lapNum: newLaps[1].lapNum, lapTime: newLaps[1].lapTime })
+      }
     }
 
     setTotalLapDuration(timeElapsed)
     setLaps(newLaps)
   }
-  
+
   const resetStopWatch = () => {
     setTimeElapsed(0)
     setLaps([])
     setTotalLapDuration(0)
+    setLongestLap({ lapNum: 0, lapTime: 0 })
+    setShortestLap({ lapNum: 0, lapTime: Number.MAX_VALUE })
   }
   
-  const displayLapLines = () => {
-    const lapsToDisplay = 5 - laps.length
-    let emptyLaps = []
-
-    for (let i = 0; i <= lapsToDisplay; i++) {
-      emptyLaps.push('')
-    } 
-
-    return emptyLaps
+  const handleLapsDisplay = () => { 
+    if (laps.length > 0) { 
+      return displayLaps(laps, longestLap, shortestLap) 
+    }
   }
-  
-  const handleStartStopButton = () => {startTime ? setStartTime(0) : startStopwatch()}
+
+  const handleEmptyLapsDisplay = () => { 
+    if (laps.length < 6) { 
+      return displayEmptyLaps(laps) 
+    }
+  }
+  const handleStartStopButton = () => startTime ? setStartTime(0) : startStopwatch()
   const handleLapResetButton = startTime ? addLap : resetStopWatch
   
   const lapResetButtonText = (startTime || !timeElapsed) ? 'Lap' : 'Reset'
   const startStopButtonText = startTime ? 'Stop' : 'Start'
   const startStopButtonClassName = startTime ? "stop" : "start"
-  
 
   return (
-    <>
-      <main>
-        <h1 className="time">{formatTime(timeElapsed)}</h1>
+    <main>
+      <h1 className="time">{formatTime(timeElapsed)}</h1>
 
-        <div className="controls">
-          <button
-            className="lap-reset-button"
-            onClick={handleLapResetButton}
-            disabled={!timeElapsed}
-          >
-            {lapResetButtonText}
-          </button>
-          <button
-            className={"start-stop-button " + startStopButtonClassName}
-            onClick={handleStartStopButton}
-          >
-            {startStopButtonText}
-          </button>
-        </div>
+      <div className="controls">
+        <button
+          className="lap-reset-button"
+          onClick={handleLapResetButton}
+          disabled={!timeElapsed}
+        >
+          {lapResetButtonText}
+        </button>
+        <button
+          className={"start-stop-button " + startStopButtonClassName}
+          onClick={handleStartStopButton}
+        >
+          {startStopButtonText}
+        </button>
+      </div>
 
-        <div className="lap-list-container">
-          <ul className="lap-list">
-            {laps.map((lap) => {
-              return (
-                <li
-                  key={lap.lapNum}
-                  className={
-                    laps.length > 2 ?
-                    lap.lapNum === longestLap.lapNum ? 'longest' : 
-                    lap.lapNum === shortestLap.lapNum ? 'shortest' :
-                    '' : ''
-                  }
-                >
-                  Lap {lap.lapNum} <span>{formatTime(lap.lapTime)}</span>
-                </li>
-              )
-            })}
-            {displayLapLines().map((item, index) => {
-              return <li key={index + 6}></li>
-            })}
-          </ul>
-        </div>
-      </main>
-    </>
+      <div className="lap-list-container">
+        <ul className="lap-list">
+          {handleLapsDisplay()}
+          {handleEmptyLapsDisplay()}
+        </ul>
+      </div>
+    </main>
   )
 }
 
